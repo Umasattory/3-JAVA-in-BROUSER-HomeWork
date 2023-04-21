aad645935725e5c893f9791efa27277f3b2711c3
Umed Sattorov <umasattory@gmail.com>
2023-04-21 13:12:02 +0300

    Updated 21/04/2023

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,22 +1,20 @@
 
+const mainMenu = Array.from(document.querySelectorAll('.menu__link'))
+const hasSubMenu = mainMenu.filter((hasSub) => hasSub.nextElementSibling !== null);
 
-const menuLinks = Array.from(document.querySelectorAll('.menu__link'));
-const hasSubMenu = menuLinks.filter((link) => link.nextElementSibling !== null);
-hasSubMenu.forEach((link) => {
-   link.onclick = () => {
-      if (link.closest('.menu__item').querySelector('.menu_sub')) {
+hasSubMenu.forEach((subs) => {
+   subs.onclick = () => {
+      if (subs.closest('.menu__item').querySelector('.menu_sub')) {
          if (document.querySelector('.menu_active')) {
             document.querySelector('.menu_active').classList.remove('menu_active')
          }
-         link.closest('.menu__item').querySelector('.menu_sub').classList.add('menu_active');
-      };
+         subs.closest('.menu__item').querySelector('.menu_sub').classList.add('menu_active');
+      }
+      return false;
    }
-});
+})
 
-
-const allmenuLink = Array.from(document.querySelectorAll('.menu__link'));
+/* const allmenuLink = Array.from(document.querySelectorAll('.menu__link'));
 allmenuLink.forEach((element) => {
    const b = a.filter((element) => element.nextElementSibling !== null)
-//
-*/
-
+}) */
