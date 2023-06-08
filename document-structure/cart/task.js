bebe440c5c891cd8c3718f0952b1af45d335d8b6
Umed Sattorov <umasattory@gmail.com>
2023-06-08 20:50:22 +0300

    upgrade 08/06/2023

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,65 +1,73 @@
-const dec = Array.from(document.querySelectorAll('.product__quantity-control_dec')); //-
-const inc = Array.from(document.querySelectorAll('.product__quantity-control_inc')); //+
-const addBascetBtn = Array.from(document.querySelectorAll('.product__add'));
+const quantity_Btn = Array.from(document.querySelectorAll('.product__quantity-control'))
+const addBascet_Btn = Array.from(document.querySelectorAll('.product__add'));
+//-----------------------------------------------------------//
+const cart_Block = document.querySelector('.cart'); // Блок корзины
+//const cart_list = Array.from(cart_Block.querySelectorAll(".cart__product")); // Массив корзины
+const cart_clear = cart_Block.querySelector('#clearCarts') // Отчистка карзины
+//-----------------------------------------------------------//
+console.log();
 let controlArray = [];
-//-----------------------------------------------------------//
-const cartBlock = document.querySelector('.cart'); // Блок корзины
-//-----------------------------------------------------------//
 
-inc.forEach((item) => {
+quantity_Btn.forEach((item) => {
    item.addEventListener('click', () => {
-      const prodQuantity = item.closest('.product__quantity-controls').querySelector('.product__quantity-value');
-      prodQuantity.innerText++;
-   })
-})
-
-dec.forEach((item) => {
-   item.addEventListener('click', () => {
-      const prodQuantity = item.closest('.product__quantity-controls').querySelector('.product__quantity-value');
-      if (prodQuantity.innerText > 1) {
-         prodQuantity.innerText--;
+      if (item.classList.contains('product__quantity-control_inc')) {
+         const prodQuantity = item.closest('.product__quantity-controls').querySelector('.product__quantity-value');
+         prodQuantity.innerText++;
       } else {
-         prodQuantity.innerText = 0;;
+         const prodQuantity = item.closest('.product__quantity-controls').querySelector('.product__quantity-value')
+         if (prodQuantity.innerText > 1) {
+            prodQuantity.innerText--;
+         } else {
+            prodQuantity.innerText = 0;
+         }
       }
    })
 })
 
-addBascetBtn.forEach((item) => {
+cart_clear.addEventListener('click', () => {
+   const defoultCart = document.createElement('div')
+   defoultCart.classList = 'cart__product';
+   defoultCart.dataset.id = '0';
+   defoultCart.insertAdjacentHTML("afterbegin", '<img class="cart__product-image" src="image.png"><div class="cart__product-count">0</div');
+
+   const clear = cart_Block.querySelector('.cart__products');
+   while (clear.firstChild) {
+      clear.removeChild(clear.lastChild);
+   }
+
+   cart_Block.querySelector('.cart__products').appendChild(defoultCart);
+})
+
+addBascet_Btn.forEach((item) => {
    item.addEventListener('click', () => {
-      addToBascet(item);
+      bascetWork(item);
    })
 });
 
-//----------------------------------------------------------------------------//
-function addToBascet(element) {
+
+
+function bascetWork(element) {
    const prod_id = element.closest('.product').dataset.id; // id Продукта
    const prod_img = element.closest('.product').querySelector('.product__image').src; // Картинка продукта
    const prod_Quantity_val = element.closest('.product').querySelector('.product__quantity-value').innerText; // количество продукта
 
-   if (cartBlock.querySelector('.cart__product').dataset.id == 0) {
-      controlArray.push(prod_id);
-      cartBlock.querySelector('.cart__product').dataset.id = prod_id;
-      cartBlock.querySelector('img').src = prod_img;
-      cartBlock.querySelector('.cart__product-count').innerText = prod_Quantity_val;
-   } else if (cartBlock.querySelector('.cart__product').dataset.id == prod_id) {
-      cartBlock.querySelector('.cart__product-count').innerText = prod_Quantity_val;
-   } else if (cartBlock.querySelector('.cart__product').dataset.id !== prod_id) {
-//-------------------поиск на присутствие такой же id в корзине------------------------//
-      if (controlArray.includes(prod_id)) {
-         const prod_list = Array.from(cartBlock.querySelectorAll('.cart__product'));
-         const findID_for_val = prod_list.filter((elem) => elem.dataset.id == prod_id);
-         //findID_for-val.querySelector('.cart__product-count').innerText = prod_Quantity_val; //- ПРОБЛЕМА
-         console.log(findID_for_val);
+   if (cart_Block.querySelector('.cart__product').dataset.id == 0) {
+      cart_Block.querySelector('.cart__product').dataset.id = prod_id;
+      cart_Block.querySelector('.cart__product-image').src = prod_img;
+      cart_Block.querySelector('.cart__product-count').innerText = prod_Quantity_val;
+   } else {
+      const findNeed = cart_Block.querySelector('.cart__products').querySelector(`'[data-id="${prod_id}"]'`);
+      if (findNeed) {
+         findNeed.querySelector('.cart__product-count').innerText = prod_Quantity_val;
       } else {
-//--------------------------создание клона нового товара--------------------------------//
-         controlArray.push(prod_id);
-         const newBlock = cartBlock.querySelector('.cart__product').cloneNode(true);
+         const newBlock = cart_Block.querySelector('.cart__product').cloneNode(true);
          newBlock.dataset.id = prod_id;
          newBlock.querySelector('img').src = prod_img;
          newBlock.querySelector('.cart__product-count').innerText = prod_Quantity_val;
-//---------------------------вставка нового блока товара--------------------------------//
-         const listProds = cartBlock.querySelector('.cart__products');
+         const listProds = cart_Block.querySelector('.cart__products');
          listProds.insertBefore(newBlock, null);
       }
    }
 };
+
+//cart_Block.querySelector('.cart__product-count').innerText = prod_Quantity_val;
